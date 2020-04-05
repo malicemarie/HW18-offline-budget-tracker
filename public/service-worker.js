7ce72dd340be29cea6739efd0dd03b023a8a8257
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-04-04 21:42:12 -0700

    service worker updated and working I think

---
+++ public/service-worker.js
@@ -0,0 +1,77 @@
+const FILES_TO_CACHE = [
+  `/`,
+  `/index.html`,
+  `/favicon.ico`,
+  `/manifest.webmanifest`,
+  `/assests/css/style.css`
+];
+
+const CACHE_NAME = `static-cache-v2`;
+const DATA_CACHE_NAME = `data-cache-v1`;
+
+// install
+self.addEventListener(`install`, event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log(`Your files were pre-cached successfully!`);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener(`activate`, evt => {
+  evt.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log(`Removing old cache data`, key);
+            return caches.delete(key);
+          }
+          return undefined;
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener(`fetch`, evt => {
+  if (evt.request.url.includes(`/api/`)) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(cache =>
+          fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            // Network request failed, try to get it from the cache.
+            .catch(() => cache.match(evt.request))
+        )
+        .catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+    caches
+      .open(CACHE_NAME)
+      .then(cache =>
+        cache
+          .match(evt.request)
+          .then(response => response || fetch(evt.request))
+      )
+  );
+});
